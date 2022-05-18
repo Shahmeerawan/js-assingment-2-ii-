64429b34b174f7ad26b685e58f50629c576f5968
Shahmeer Hussain <hussainshahmeer99@gmail.com>
2022-05-18 04:58:41 -0700

    added js_assingment (ii)
    added js_assingment (ii)

---
+++ app.js
@@ -0,0 +1,55 @@
+//*****************TASK  # 03*******************/
+
+
+// var name = prompt ("Enter yor name")
+// alert("Thnak you for Visiting our site")
+
+
+
+
+
+//*****************TASK  # 04*******************/
+
+
+//*****************TASK  # 05*******************/
+
+// var n = +prompt("Enter any number")
+// if(" " === "5"){
+//     5 + "x" + 1 + "=" + 5 * 1 + "<br>" + 5 + "x" + 2 + "=" + 5 * 2 + "<br>"  + 5 + "x" + 3 + "=" + 5 * 3 + "<br>"  + 5 + "x" + 4 + "=" + 5 * 4 + "<br>"  + 5 + "x" + 5 + "=" + 5 * 5 + "<br>"  + 5 + "x" + 6 + "=" + 5 * 6 + "<br>"  + 5 + "x" + 7 + "=" + 5 * 7 + "<br>"  + 5 + "x" + 8 + "=" + 5 * 8 + "<br>"  + 5 + "x" + 9 + "=" + 5 * 9 + "<br>"  + 5 + "x" + 10 + "=" + 5 * 10 + "<br>"
+
+// }
+// document.write("<h2>" + n + "</h2>");
+
+// document.write (n + "x" + 1 + "=" + n * 1 + "<br>" + n + "x" + 2 + "=" + n * 2 + "<br>"  + n + "x" + 3 + "=" + n * 3 + "<br>"  + n + "x" + 4 + "=" + n * 4 + "<br>"  + n + "x" + 5 + "=" + n * 5 + "<br>"  + n + "x" + 6 + "=" + n * 6 + "<br>"  + n + "x" + 7 + "=" + n * 7 + "<br>"  + n + "x" + 8 + "=" + n * 8 + "<br>"  + n + "x" + 9 + "=" + n * 9 + "<br>"  + n + "x" + 10 + "=" + n * 10 + "<br>")
+
+
+
+
+
+//*****************TASK  # 06*******************/
+
+// var sub1 = prompt("Enter your First Subject");
+// var sub2 = prompt("Enter your second Subject");
+// var sub3 = prompt("Enter your third Subject");
+
+// var totalmarks = 300 ;
+// var onesubmarks = 100 ;
+
+// var om1 = +prompt("enter your obtined marks in first subjct")
+// var om2 = +prompt("enter your obtined marks in second subjct")
+// var om3 = +prompt("enter your obtined marks in third subjct")
+// var tob = om1 + om2 + om3
+
+
+// var res1 = (om1 / onesubmarks) * 100;
+// var res2 = (om2 / onesubmarks) * 100;
+// var res3 = (om3 / onesubmarks) * 100;
+// var tres = (tob / totalmarks ) * 100 ;
+
+// document.write
+// document.write( sub1 + "    " + onesubmarks + "    " + om1 + "  " + res1 + "%" + "<br>");
+// document.write( sub2 + "    "   + onesubmarks + "    " + om2 +res2 + "%" + "<br>");
+// document.write( sub3 + "    " + onesubmarks + "    " + om3 +res3 + "%" + "<br>");
+
+// document.write("<h2>" +  +  tob +  + " " + tres + " " )
+
